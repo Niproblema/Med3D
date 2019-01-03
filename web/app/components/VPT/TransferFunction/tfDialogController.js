2111192db942ccf588df381f4b2339be2408ca8b
Jan Aleksandrov <jan.aleksandrov@gmail.com>
2019-01-03 07:27:35 +0100

    Revert camera plan

--- web/app/components/VPT/TransferFunction/tfDialogController.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by Jan on 23.11.2018.
- */
-
-var tfDialogController = function($scope, PublicRenderData) {
-    $scope.publicRenderData = PublicRenderData;
-};
-
-app.controller('TFDialogController', tfDialogController);
